599f71e7744632ea51d5d362d5d732282af8d181
akirafuwa <akirafuwa@gmail.com>
2023-06-04 05:23:15 +0900

    initial commit

---
+++ first/Main.java
@@ -0,0 +1,25 @@
+package first;
+
+// Problem: Counting duplicate characters
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        String test = "aabbccTasdf";
+
+
+    }
+
+    public HashMap<String, Integer> countDuplicateCharacters(String input){
+        HashMap<String, Integer> result = new HashMap<>();
+        int currentLocation = 0;
+
+        for (int i = 0; i < input.length(); i++){
+            Char subject = input.charAt(i);
+
+        }
+
+        return result
+    }
+}
